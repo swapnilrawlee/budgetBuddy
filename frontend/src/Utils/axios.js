fe6822e3c467513a64a7806ccf225682c82718d0
Swapnil Rawle <swapnilrawle3000@gmail.com>
2024-12-22 10:06:02 +0530

    axios changed again with new render deploy

--- frontend/src/Utils/axios.js
+++ frontend/src/Utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
  const axiosInstance  = axios.create({
-    baseURL: 'https://budgetbuddy-21kb.onrender.com/', 
+    baseURL: 'https://budgetbuddy-rctg.onrender.com', 
 });
 
 export default axiosInstance;
