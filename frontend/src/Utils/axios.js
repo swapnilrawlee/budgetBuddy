2ce43fccf949737e4e844fbe2e73cf3dbb4804c2
Swapnil <swapnilrawle3000@gmail.com>
2025-04-14 21:38:39 +0530

    version change

--- frontend/src/Utils/axios.js
+++ frontend/src/Utils/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
  const axiosInstance  = axios.create({
-    baseURL: 'https://budgetbuddy-rctg.onrender.com', 
-    // baseURL: 'http://localhost:3000',  // For local development
+    // baseURL: 'https://budgetbuddy-rctg.onrender.com', 
+    baseURL: 'http://localhost:3000',  // For local development
  
 });
 
